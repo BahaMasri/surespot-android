40b86a77f39a04314a155ddfb437bb3902d60046
Zoff <zoff99@users.noreply.github.com>
2016-02-12 11:02:53 +0100

    fix

--- surespot/src/main/java/org/bitcoinj/core/Sha256Hash.java
+++ surespot/src/main/java/org/bitcoinj/core/Sha256Hash.java
@@ -21,8 +21,8 @@
 
 package org.bitcoinj.core;
 
-// import com.google.common.io.ByteStreams;
-// import com.google.common.primitives.*;
+import com.google.common.io.ByteStreams;
+import com.google.common.primitives.*;
 
 import java.io.File;
 import java.io.FileInputStream;
