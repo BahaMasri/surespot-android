bdbc2b21ea124feeb533d22fa48dfc4761284aaa
Zoff <zoff99@users.noreply.github.com>
2016-02-20 11:07:19 +0100

    typo

--- surespot/src/main/java/com/twofours/surespot/activities/MainActivity.java
+++ surespot/src/main/java/com/twofours/surespot/activities/MainActivity.java
@@ -10,7 +10,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;r
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
